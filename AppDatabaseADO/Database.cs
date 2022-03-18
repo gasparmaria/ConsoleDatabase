91f75d210728e6e1d4e2fb80e78bfdfe85ece9f0
Maria Eduarda Gaspar <73003909+gasparmaria@users.noreply.github.com>
2022-03-18 11:33:25 -0300

    Add files via upload

---
+++ AppDatabaseADO/Database.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+
+namespace ConsoleDatabase
+{
+    public class Database : IDisposable
+    {
+        private readonly MySqlConnection conexao;
+
+        public Database()
+        {
+            conexao = new MySqlConnection(ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString);
+            conexao.Open();
+        }
+
+        public void ExecuteCommand(string query)
+        {
+            var command = new MySqlCommand
+            {
+                CommandText = query,
+                CommandType = CommandType.Text,
+                Connection = conexao
+            };
+            command.ExecuteNonQuery();
+        }
+
+        public MySqlDataReader ReturnCommand(string query)
+        {
+            var command = new MySqlCommand(query, conexao);
+            return command.ExecuteReader();
+        }
+
+        public void Dispose()
+        {
+            if (conexao.State == ConnectionState.Open)
+                conexao.Close();
+        }
+    }
+}
